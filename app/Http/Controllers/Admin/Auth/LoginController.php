6b45adb458faa6b0ebdde1a3531e2febf7897c76
josephkat <zecakat@gmail.com>
2022-01-09 17:26:50 -0300

    adding loging function

--- app/Http/Controllers/Admin/Auth/LoginController.php
+++ app/Http/Controllers/Admin/Auth/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
 
 class LoginController extends Controller
@@ -16,12 +17,34 @@ class LoginController extends Controller
         return view('admin.login');
     }
 
-    public function authenticate() {
-        return view('admin.login');
+    public function authenticate(Request $request) {
+        $data = $request->only([
+            'email',
+            'password',
+            'remember'
+        ]);
+        $validator = $this->validator($data);
+
+        if($validator->fails()) {
+            return redirect()->route('login')
+            ->withErros($validator)
+            ->withInput();
+        }
+
+        if(Auth::attempt($data)) {
+            return redirect()->route('admin');
+        }
     }
 
     public function logout() {
         Auth::logout();
         return redirect()->route('login');
+    }
+
+    protected function validator(array $data) {
+        return Validator::make($data, [
+            'email' => ['required', 'string', 'email', 'max:100'],
+            'password' => ['required', 'string', 'min:4']
+        ]);
     }
 }
